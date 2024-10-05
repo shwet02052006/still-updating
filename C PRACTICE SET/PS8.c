32108931c1d1e508f4c6fe429651213c7d5f5190
shwet02052006 <shwet.9834@gmail.com>
2024-10-05 19:04:40 +0530

    .

---
+++ C PRACTICE SET/PS8.c
@@ -0,0 +1,47 @@
+// CHAPTER 8  – PRACTICE SET  
+// 1. Which of the following is used to appropriately read a multi-word string. 
+// 1. gets() correct
+// 2. puts() 
+// 3. printf() 
+// 4. scanf() 
+// 2. Write a program to take string as an input from the user using %c and %s confirm 
+// that the strings are equal. 
+//#include<stdio.h>
+// int main(){
+//     char str[6];
+//     for (int  i = 0; i < 5; i++)
+//     {
+//      scanf("%c",&str[i]);
+//      fflush(stdin);// Every time we enter the char it also calculated enter with  it for that this function is used 
+//     }
+//     str[5]='\0';
+//     printf("%s",str);
+//     return 0;
+// }
+// 3. Write your own version of strlen function from <string.h>
+//#include<stdio.h>
+
+// int main(){
+//     char str[]="shwet";
+//     int i=0,count;
+//    int c=str[i];
+// while (c!= '\0')
+// {
+//    c=str[i];
+//    i++;
+// }
+// count= i-1;
+// printf("%d",count);
+    
+//     return 0;
+// } 
+// 4. Write a function slice() to slice a string. It should change the original string such 
+// that it is now the sliced string. Take ‘m’ and ‘n’ as the start and ending position 
+// for slice. 
+// 5. Write your own version of strcpy function from <string.h> 
+// 6. Write a program to encrypt a string by adding 1 to the ascii value of its 
+// characters. 
+// 7. Write a program to decrypt the string encrypted using encrypt function in 
+// problem 6. 
+// 8. Write a program to count the occurrence of a given character in a string. 
+// 9. Write a program to check whether a given character is present in a string or not.
