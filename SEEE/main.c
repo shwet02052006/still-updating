0c8f42873f62c73499b4044e4402f79429acc7a8
shwet02052006 <shwet.9834@gmail.com>
2024-10-16 00:02:37 +0530

    .

--- SEEE/main.c
+++ SEEE/main.c
@@ -1,7 +1,27 @@
-#include<stdio.h>
-int main(){
-    int a,b;
-    scanf("%d %d",&a,&b);
-    printf("%d",a+b);
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num == 31)
+    {
+        printf("NAGPURE\n");
+    }
+    else if (num == 49)
+    {
+        printf("NAGPURE east\n");
+    }
+    else if (num == 40)
+    {
+        printf("NAGPURE rural\n");
+    }
+    else
+    {
+        printf("error\n");
+    }
+
     return 0;
 }
+
