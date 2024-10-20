3f5e59765256369a84072916f1b20341c55d7098
shwet02052006 <shwet.9834@gmail.com>
2024-10-21 01:24:10 +0530

    .

---
+++ SEEE/fibonacciele.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{ // FIB FOR 10 ELEMEMTS
+    int f1 = 1, f2 = 2, f3;
+    printf("%d \t%d", f1, f2);
+    printf("\t");
+    for (int i = 1; i <= 8; i++)
+    {
+        f3 = f1 + f2;
+
+        printf("%d\t", f3);
+        f1 = f2;
+        f2 = f3;
+    }
+
+    return 0;
+}
--- c for College/PE1.c
+++ c for College/PE1.c
@@ -1,1 +1,13 @@
-#include
+#include <stdio.h>
+int main()
+{
+    int sum = 0;
+
+    for (int i = 0; i < 1000; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+            sum = sum + i;
+    }
+    printf("Sum is %d", sum);
+    return 0;
+}
