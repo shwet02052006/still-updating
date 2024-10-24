f97ba9e09c794baa445484b5e1f6313c2aadc84c
shwet02052006 <shwet.9834@gmail.com>
2024-10-25 00:54:58 +0530

    .

--- c for College/PE3.c
+++ c for College/PE3.c
@@ -1,5 +1,20 @@
-#include<stdio.h>
-int main(){
-    
+#include <stdio.h>
+int main()
+{
+    long long num = 600851475143;
+    long long i=0;
+    while (num % 2 == 0)
+    {
+
+        num = num / 2;
+    }
+    for (i = 3; i <= num; i += 2)
+    {
+        while (num % i == 0)
+        {
+            num=num/i;
+        }
+    }
+    printf("%lld",(i-2));
     return 0;
 }
